85585d2d3b94b105a3ec783bb5339291fd0008e8
Brandon Kelly <brandonmichaelkelly@gmail.com>
2017-04-17 23:25:26 -0700

    updated function that removes the modulus to make it more simplified to read

--- ScheduleYourDa[rra]y.js
+++ ScheduleYourDa[rra]y.js
@@ -1,24 +1,21 @@
 function dayPlan (hours, tasks, duration){
-	var schedule = [];
-	var minAvailToWork = hours * 60;
-	var taskTimeNeeded = tasks * duration;
+  var schedule = [];
+  var minAvailToWork = hours * 60;
+  var taskTimeNeeded = tasks * duration;
 
-	if (taskTimeNeeded > minAvailToWork) {
-		schedule = "You're not sleeping tonight!";
-	}
-	else {
-		var breakTime = Math.round((minAvailToWork - taskTimeNeeded) / (tasks -1));
-		var workOrBreak = duration;
+  if (taskTimeNeeded <= minAvailToWork) {
+    var breakTime = Math.round((minAvailToWork - taskTimeNeeded) / (tasks - 1));
 
-		for (var i = 1; i < (tasks * 2); i++) {
-			schedule.push(workOrBreak);
-			if (i % 2 === 0) {
-				workOrBreak = duration;
-			}
-			else {
-				workOrBreak = breakTime;
-			}
-		}
-	}
-	return schedule;
+    for (var i = 1; i < tasks ; i++) {
+      schedule.push(duration);
+      schedule.push(breakTime);
+      if (tasks - i === 1) {
+        schedule.push(duration);
+      }
+    }
+  }
+  else {
+    schedule = "You're not sleeping tonight!";
+  }
+  return schedule;
 }
